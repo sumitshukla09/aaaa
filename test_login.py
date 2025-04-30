bb3c86b0436b30a099fe121f3bba931f0c14c971
root <root@695111b003d8>
2025-04-30 17:11:22 +0530

    Pushed Successfully

--- test_login.py
+++ test_login.py
@@ -1,25 +1,13 @@
+
+
 import pytest
-
-from selenium.webdriver.common.by import By
-
-
-
-class TestClass :
-    @pytest.mark.parametrize('user,pwd',
-                             [('student','Password123'),('student','password123'),('Student','Password'),('student12','password123')])
-    def test_login(self,user,pwd,setup):
-        self.driver = setup
-        self.driver.get('https://practicetestautomation.com/practice-test-login/')
-        self.driver.implicitly_wait(10)
-        self.driver.find_element(By.XPATH,'//input[@name="username"]').send_keys(user)
-        self.driver.find_element(By.XPATH,'//input[@name="password"]').send_keys(pwd)
-        self.driver.find_element(By.XPATH,'//button[@id="submit"]').click()
-        try:
-            self.status = self.driver.find_element(By.XPATH,'//h1[@class="post-title"]').is_displayed()
-            self.driver.close()
-
-            assert self.status == True
+from selenium import webdriver
+from selenium.webdriver.ie.service import Service
 
 
-        except:
-            assert False
+@pytest.fixture()
+def setup():
+    ser_obj = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=ser_obj)
+
+    return driver
